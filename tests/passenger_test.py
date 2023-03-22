8358692c009a12bf20063383138228f24d2bc841
Simson Garfinkel <simsong@acm.org>
2023-03-22 18:52:54 -0400

    added minimal passenger_test

---
+++ tests/passenger_test.py
@@ -0,0 +1,10 @@
+import sys
+import os
+from os.path import abspath,dirname
+
+sys.path.append( dirname(dirname(abspath(__file__))))
+
+import passenger_wsgi
+
+def passenger_test():
+    assert passenger_wsgi.HOME = os.getenv('HOME')
