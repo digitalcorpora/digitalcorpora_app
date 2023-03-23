c04a90c806152d8fe07b480960b6289aec31b547
Simson Garfinkel <simsong@acm.org>
2023-03-22 22:24:50 -0400

    more tests for s3_reports

--- tests/reports_test.py
+++ tests/reports_test.py
@@ -6,16 +6,29 @@ from os.path import abspath,dirname
 
 sys.path.append( dirname(dirname(abspath(__file__))))
 
+from boddle import boddle
+
 import s3_reports
 import bottle_app
 
 def test_report_count():
     assert s3_reports.report_count() == len(s3_reports.REPORTS)
 
-def test_reports_generate():
+def test_reports_json():
     dbreader = bottle_app.get_dbreader()
-    ret = s3_reports.report_generate(auth=dbreader, num=0)
-    assert 'title' in ret
-    assert 'sql' in ret
-    assert 'column_names' in ret
-    assert 'rows' in ret
+    # Make sure each report works without error
+    for i in range(s3_reports.report_count()):
+        ret = s3_reports.report_generate(auth=dbreader, num=i)
+        assert 'title' in ret
+        assert 'sql' in ret
+        assert 'column_names' in ret
+        assert 'rows' in ret
+        ret = s3_reports.report_json(auth=dbreader, num=i)
+
+def test_reports_html():
+    dbreader = bottle_app.get_dbreader()
+    with boddle(params={'report':'0'}):
+        res = s3_reports.reports_html(auth=dbreader)
+
+    with boddle(params={}):
+        res = s3_reports.reports_html(auth=dbreader)
